72e94c2ec112e6be388ba30cf5bb2e7d6de67aca
IPreferWater <ipreferwater2@gmail.com>
2020-09-09 11:33:38 +0200

    feat(develop): add number of participants

--- components/planningday.js
+++ components/planningday.js
@@ -9,11 +9,21 @@ function getColorType(type){
       return "bg-gray-200"
   }
 }
+
+function getParticipants(participants){
+  if ( null == participants ){
+    return ""
+  }
+  return `${participants.booked} / ${participants.max} places`
+}
 export function PlanningDay({day}) {
   return(<div className="min-h-200 min-w-200 rounded overflow-hidden shadow-lg relative">
 
 <div className="px-6 py-4 pb-16">
+  <div className="flex flex-row justify-between">
     <div className="font-bold text-xl mb-2">{day.day}</div>
+    <div className="text-xs">{getParticipants(day.participants)}</div>
+    </div>
     <ul className="min-h-32 min-w-32">
     {day.labels.map((label, i) =>
     <li key={i} className="text-gray-700 text-base">
@@ -22,6 +32,8 @@ export function PlanningDay({day}) {
     )}
     </ul>
 
+      
+
   </div>
   <div className="absolute inset-x-0 bottom-0 px-6 pt-4 pb-2">
     <span className={`inline-block ${getColorType(day.type)} rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}>{`#${day.type}`}</span>
@@ -29,28 +41,5 @@ export function PlanningDay({day}) {
 
   </div>)
 }
- /* return (<div>
-    <h1 className="text-2xl">{day.day}</h1>
-    <h2 className="text-2xl">{day.type}</h2>
-    <ul>
-    {day.labels.map((label, i) =>
-    <li key={i}>{label}</li>
-    )}
-    </ul>
-  </div> 
-  );
-} */
-//PlanningDay.PropTypes.arrayOf(string)
-/*PlanningDay.propTypes = {
-  title: PropTypes.string,
-  benefits: PropTypes.arrayOf(
-    PropTypes.shape({
-      benefits: PropTypes.string
-    })
-  )
-};*/
-/*PlanningDay.propTypes = {
-  labels: PropTypes.arrayOf(PropTypes.string)
-};*/
 
 
--- pages/plannings/[slug].js
+++ pages/plannings/[slug].js
@@ -1,5 +1,4 @@
 import { getPlanningBySlug, getAllPlannings } from "@api"
-//import { Footer} from "../../components/footer"
 import { PlanningDay } from "@components/planningday"
 
 export default function Planning(props){
@@ -10,7 +9,6 @@ export default function Planning(props){
                 <h1>{data.label}</h1>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">                {data.planning.map((day, index) => (
                     <PlanningDay  key={index} day={day}/>
-                   
     ))}
     </div>
                 
