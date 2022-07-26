9eb455a78fe8d6b54f1880dd95b2dab39003546b
IPreferWater <ipreferwater2@gmail.com>
2022-07-26 15:32:17 +0200

    feat(v2): make a green grid for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,24 @@ type IndexProps = {
   return (<Layout title="index">
     <h1>Index</h1>
    
+   {/* latest creations */}
+
+   <div className='w-4/5 m-auto h-96 flex flex-col background-grid-green '>
+    <div>logo new</div>
+    <div className='flex flex-row'>
+      <div className='bg-blue-300'>
+        <img className='max-h-48' src='creations/kimono/c_2.webp'/>
+        <h1>title</h1>
+      </div>
+
+      <div className='bg-blue-300 w-full'>
+        <img className='max-h-48' src='creations/kimono/c_2.webp'/>
+      </div>
+    </div>
+
+    <div>link all creations</div>
+
+   </div>
   </Layout>)
 }
 
