f76e59776dfd0b5c400f1c48201575a51090add0
IPreferWater <ipreferwater2@gmail.com>
2022-12-11 18:56:18 +0100

    feat(v2): change images OU to webp, add key to arr Ou component

--- components/ou/Item.tsx
+++ components/ou/Item.tsx
@@ -14,8 +14,8 @@ export const Item = ( {title, txts, img} : ItemProps) => {
 return <div className='flex flex-col gap-y-4'>
   {title!=null ? <h1 className='text-2xl font-bold'>{title}</h1>:null}
   {//render sentences if defined
-txts!=null ? txts.map((str: string) => {
-  return <p className='text-slate-700'>{str}</p>
+txts!=null ? txts.map((str: string,i: number) => {
+  return <p className='text-slate-700' key={i}>{str}</p>
 }) : null
 }
   {img!=null ? <img className='object-contain h-72' src={img}/>:null}
--- pages/ou.tsx
+++ pages/ou.tsx
@@ -15,16 +15,16 @@ const OuPage = () => (
 
    <Item title='Oui mais c’est où ?!' txts={['l’atelier est situé dans une petite servitude']}/>
 
-   <Item  txts={['Venant de Rue Gaston Barrate / Boulevard Montalembert']} img='ou/ou_from_gaston_baratte.jpg'/>
-   <Item  txts={['Venant de Rue Kléber']} img='ou/ou_from_kleber.jpg'/>
-   <Item  txts={['Dans cette servitude, la 3eme maison sur votre droite']} img='ou/ou_garage.jpg'/>
-   <Item  txts={['Vous ne pouvez pas louper la pancarte sur la porte !']} img='ou/ou_pancarte.jpg'/>
+   <Item  txts={['Venant de Rue Gaston Barrate / Boulevard Montalembert']} img='ou/ou_from_gaston_baratte.webp'/>
+   <Item  txts={['Venant de Rue Kléber']} img='ou/ou_from_kleber.webp'/>
+   <Item  txts={['Dans cette servitude, la 3eme maison sur votre droite']} img='ou/ou_garage.webp'/>
+   <Item  txts={['Vous ne pouvez pas louper la pancarte sur la porte !']} img='ou/ou_pancarte.webp'/>
    <p className='text-xs text-zinc-500'>(sauf si j’ai l’ai laissé ouverte ... )</p>
    <Item title='Accéder'/>
-   <Item title='Vélo'  txts={['Vous pouvez laisser votre vélo sur la terasse devant l’atelier, vous pourez ainsi surveiller qu’il ne fugue pas', 'Si vous avez peur qu’il attrape froid avec la pluie nous le mettrons au Garage avec le mien ']} img='ou/ou_interieur.jpg'/>
+   <Item title='Vélo'  txts={['Vous pouvez laisser votre vélo sur la terasse devant l’atelier, vous pourez ainsi surveiller qu’il ne fugue pas', 'Si vous avez peur qu’il attrape froid avec la pluie nous le mettrons au Garage avec le mien ']} img='ou/ou_interieur.webp'/>
    
    <p className='text-xs text-zinc-500'>(merci de ne ramener que des vélos bien dressés pour ne pas embêter les voisins)</p>
-   <Item title='Train' txts={['La Gare d’ascq  est à 350m à pied de l’atelier et à 15 minutes de Lille Flandre']} img='ou/ou_train.jpg'/>
+   <Item title='Train' txts={['La Gare d’ascq  est à 350m à pied de l’atelier et à 15 minutes de Lille Flandre']} img='ou/ou_train.webp'/>
    <Item title='Bus' txts={['Vous pouvez prendre les lignes 13 - 73 - 910 La 73 et 910 passent par le metro Pont-de-bois', 'Horaires']}/>
     <div className='flex flex-row content-center '>
     <a className='w-full underline decoration-cel-orange decoration-2' href='https://www.ilevia.fr/pdf/PDF_Lignes/fiche-13.pdf' target='_blank'>Ligne 13</a>
@@ -34,8 +34,8 @@ const OuPage = () => (
 
     <Item title='Voiture' txts={['Il y a une place de disponible devant le garage de l’atelier']}/>
 
-    <Item txts={['La place est reservée aux particpants de l’atelier Si une personne est deja arrivée des places sont disponibles :', 'Rue Masséna']} img='ou/ou_parking_1.jpg'/>
-    <Item txts={['Rue des Martyrs']} img='ou/ou_parking_2.jpg'/>
+    <Item txts={['La place est reservée aux particpants de l’atelier Si une personne est deja arrivée des places sont disponibles :', 'Rue Masséna']} img='ou/ou_parking_1.webp'/>
+    <Item txts={['Rue des Martyrs']} img='ou/ou_parking_2.webp'/>
    </div>
 
 
