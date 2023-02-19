20c1ed3c133c411e8fc8b5029e3035fc03004172
supercaliflipette <cverein.professionnel@yahoo.fr>
2023-02-19 15:04:46 +0100

    feat(creations-page): change creations title

--- pages/creations.tsx
+++ pages/creations.tsx
@@ -12,8 +12,8 @@ import {Layout} from '../components/Layout'
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
 
     <SingleCreation creationTitle="Kimono Unique" imgUrl="https://static.cnews.fr/sites/default/files/styles/image_750_422/public/gigi_williams_366676_copier-taille1200_6202ada544ad4.jpg?itok=ThAuDQGk" price= {12}/>
-    <SingleCreation creationTitle="chapo pa bo" imgUrl=''price={100}/>
-    <SingleCreation creationTitle="mathieu merci" imgUrl=''price={300}/>
+    <SingleCreation creationTitle="béret anglais" imgUrl=''price={100}/>
+    <SingleCreation creationTitle="chouchou" imgUrl=''price={300}/>
         
   </div>
   </div>
