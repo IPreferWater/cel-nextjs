25cf1d965d017d025568c74385c1116416dd7f4c
IPreferWater <ipreferwater2@gmail.com>
2023-03-03 15:16:20 +0100

    feat(creation-page): map datas for the page

--- pages/creations.tsx
+++ pages/creations.tsx
@@ -10,17 +10,14 @@ type CreationsPageProps = {
 }
 
   export default function CreationsPage({creations} : CreationsPageProps) {
-
+    const numbers = [1, 2, 3, 4, 5];
+    
   return (<Layout title="Clementinestla créations"  metaName = "Page d'attente" metaDescription="Page regroupant toutes les créations upcyclés de Clementinestla">
    <Bandeau txt='Blablabla les creations'/>
 
     <div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-
-    <SingleCreation creationTitle="Kimono Unique" imgUrl="https://static.cnews.fr/sites/default/files/styles/image_750_422/public/gigi_williams_366676_copier-taille1200_6202ada544ad4.jpg?itok=ThAuDQGk" price= {12}/>
-    <SingleCreation creationTitle="béret anglais" imgUrl=''price={100}/>
-    <SingleCreation creationTitle="chouchou" imgUrl=''price={300}/>
-
+      {creations.map((creation) =>  <SingleCreation creationTitle={creation.title} imgUrl={creation.imgs[0]} price={0}/>)}
   </div>
   </div>
   </Layout>)
@@ -28,11 +25,11 @@ type CreationsPageProps = {
 }
 
 export async function getStaticProps(){
-  const allCreations = await getAllCreationsSortedByDate()
-  console.log(allCreations)
+  const creations = await getAllCreationsSortedByDate()
+
   return {
       props: {
-        allCreations
+        creations
       }
   }
 }
