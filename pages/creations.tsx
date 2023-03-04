dc40299c6bd8976f5cb3f56df2f3118cacb20e44
supercaliflipette <cverein.professionnel@yahoo.fr>
2023-03-04 18:07:06 +0100

    feat(creations-page):

--- pages/creations.tsx
+++ pages/creations.tsx
@@ -12,14 +12,14 @@ type CreationsPageProps = {
   export default function CreationsPage({creations} : CreationsPageProps) {
 
   return (<Layout title="Clementinestla créations"  metaName = "Page d'attente" metaDescription="Page regroupant toutes les créations upcyclés de Clementinestla">
-   <Bandeau txt='Blablabla les creations'/>
+   <Bandeau txt='Créations upcylées'/>
 
     <div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-20 mx-12'>
 
-    <SingleCreation creationTitle="Kimono Unique" imgUrl="https://static.cnews.fr/sites/default/files/styles/image_750_422/public/gigi_williams_366676_copier-taille1200_6202ada544ad4.jpg?itok=ThAuDQGk" price= {12}/>
-    <SingleCreation creationTitle="béret anglais" imgUrl=''price={100}/>
-    <SingleCreation creationTitle="chouchou" imgUrl=''price={300}/>
+    <SingleCreation creationTitle="Veste Kimono Kuzco" imgUrl="/creations/kimono/c_1.webp" url="/creations/kimono" price= {12}/>
+    <SingleCreation creationTitle="Jupe droite Coco" imgUrl='/creations/jujitsu-trou-pantalon/c_1.webp'  url="/creations/kimono"price={100}/>
+    <SingleCreation creationTitle="Short/ caleçon Parrot" imgUrl='/creations/kimono/c_1.webp'  url="/creations/kimono"price={300}/>
 
   </div>
   </div>
